2607a8fe7d91aac74e49f85d8c6fd175f9661fa5
semuelabidondifu <semueabidondifu@gmail.com>
2024-08-29 20:01:14 +0900

    simpan file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// single line coment
+
+/*
+multiple line comment
+bisa tulis juga disini
+*/
+
+
+var namadepan = "semuel";
+var namabelakang = "abidondifu";
+
+document.getElementById("nama")
+.innerHTML =  namadepan + namabelakang
